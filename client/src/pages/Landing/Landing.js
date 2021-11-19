9e5e8aeceaaabd11958c38037e1c04106f383c77
Sean Yang <seanyixuan@gmail.com>
2021-11-19 09:27:07 -0800

    created game banner announcement section

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -2,12 +2,14 @@ import React from "react";
 // import { Link } from "react-router-dom";
 import "./Landing.scss";
 import NFTContainer from "../../components/NFTContainer/NFTContainer";
+import GameBanner from "../../components/GameBanner/GameBanner";
 
 
 export default function Landing() {
   return (
     <div className="landing">
         <NFTContainer />
+        <GameBanner />
     </div>
   );
 }
