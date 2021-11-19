121180ea20b6e960a4119d2be032e5f438638475
Jesse Sanchez <jesse@henosishospitality.com>
2021-11-19 02:52:15 -0500

    added hero, and some styling

---
+++ client/src/components/Hero/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Hero.scss";
+
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div>
+      <section className="hero-container">
+        <h1 className="hero-title">
+          Enter for a Chance to Obtain Our Limited Edition Robinhood NFC!
+        </h1>
+      </section>
+    </div>
+  );
+}
---
+++ client/src/pages/Landing/Landing.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import Hero from "../../components/Hero/Hero";
+
+import axios from "axios";
+
+export default function Landing() {
+  return <div></div>;
+}
