58d2465fb4d273819c21a6c66a72a6cb35d0bdf8
Sean Yang <seanyixuan@gmail.com>
2021-11-19 09:18:42 -0800

    added missing part to landing page

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,9 +1,13 @@
 import React from "react";
+// import { Link } from "react-router-dom";
+import "./Landing.scss";
+import NFTContainer from "../../components/NFTContainer/NFTContainer";
+
 
 export default function Landing() {
   return (
-    <div>
-      <h1>Landing Page</h1>
+    <div className="landing">
+        <NFTContainer />
     </div>
   );
 }
