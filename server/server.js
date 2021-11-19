4c77a16ae8bb83c0b1bc7b0763e16feb4711ee1a
Sean Yang <seanyixuan@gmail.com>
2021-11-18 18:26:53 -0800

    fixed gitignore, established routes, created header, footer and landing components

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+// const path = require("path");
+
+const cors = require("cors");
+// const { PORT, BACKEND_URL } = process.env;
+const port = process.env.PORT || 8080;
+require('dotenv').config();
+// init the express app in order to use express methods
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.listen(port, () => {
+    console.log(`Port ${port}`);
+  });
