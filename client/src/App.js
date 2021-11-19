283267c9ef6e98f85247b0e6ca63d1625cc49f38
Andrew Stevenson <email>
2021-11-19 12:14:40 -0500

    Adding boiler-plate for carousel component

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,16 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Landing from "./pages/Landing/Landing";
 import Header from "./components/Header/Header";
+import Carousel from "./components/Carousel/Carousel";
+import NTFContainer from "./components/NFTContainer/NFTContainer";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
+        <Carousel />
+        <NTFContainer />
         <Switch>
           <Route
             exact
---
+++ client/src/components/Carousel/Carousel.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import "./Carousel.scss";
+
+export default function Carousel() {
+  return (
+    <div className="footer">
+      <h1 className="footer__h1">Carousel</h1>
+    </div>
+  );
+}
