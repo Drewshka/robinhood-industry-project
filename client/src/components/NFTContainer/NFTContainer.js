ae8e54cb034cb1cf7fb184f115c90117c7b41600
Sean Yang <seanyixuan@gmail.com>
2021-11-18 23:32:12 -0800

    added background colors to cards and added placeholder text

--- client/src/components/NFTContainer/NFTContainer.js
+++ client/src/components/NFTContainer/NFTContainer.js
@@ -10,8 +10,8 @@ export default function NTFContainer() {
             <h1 class="container__title-box-text">Hawaii</h1>
         </div> */}
         <div className="card-container">
-            <div className="card"> 
-                <h2 className="card__title">Friday</h2>
+            <div className="card card-left"> 
+                <h2 className="card__title">digital art NFT</h2>
                 {/* <ul class="card__list">
                     <li class="card__item">Breakfast</li>
                     <li class="card__item">Lunch</li>
@@ -19,8 +19,8 @@ export default function NTFContainer() {
                     <li class="card__item">Snorkeling</li>
                 </ul> */}
             </div>        
-            <div className="card"> 
-                <h2 className="card__title">Saturday</h2>
+            <div className="card card-center"> 
+                <h2 className="card__title">music NFT</h2>
                 {/* <ul class="card__list">
                     <li class="card__item">Breakfast</li>
                     <li class="card__item">Lunch</li>
@@ -28,8 +28,8 @@ export default function NTFContainer() {
                     <li class="card__item">Snorkeling</li>
                 </ul> */}
             </div>
-            <div className="card">   
-                <h2 className="card__title">Sunday</h2>
+            <div className="card card-right">   
+                <h2 className="card__title">Video or GIF NFT</h2>
                 {/* <ul class="card__list">
                     <li class="card__item">Breakfast</li>
                     <li class="card__item">Lunch</li>
